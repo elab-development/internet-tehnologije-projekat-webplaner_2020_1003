1dc6251598da1f2421c1f33bb858bb5cf72f3f6d
sicktouch037 <dakimba@gmail.com>
2024-01-11 17:37:36 +0100

    kreiranje api ruta i kontrolera

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users',[UserController::class, 'index']);
+Route::get('/users/{id}',[UserController::class, 'show']);
+
+Route::resource('planers', PlanerController::class);
+
