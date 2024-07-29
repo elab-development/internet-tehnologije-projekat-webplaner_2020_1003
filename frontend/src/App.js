45cbd8c4a335a7a2de38ea8541769964793c0068
ivana-jovanovic <ivanajovanovic.ivanar@gmail.com>
2024-07-29 15:04:02 +0200

    kreirana reusable komponenta footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Pocetna from './components/Pocetna';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
         <Pocetna></Pocetna>
+        <Footer></Footer>
     </div>
   );
 }
