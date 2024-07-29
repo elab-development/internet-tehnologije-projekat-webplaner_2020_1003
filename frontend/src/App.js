b0ad0d90578be635affd8b85b037ad0dad749d0d
david2020-1003 <dakimba@gmail.com>
2024-07-29 16:29:20 +0200

    rute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Pocetna from './components/Pocetna';
-import Footer from './components/Footer';
 import Register from './components/Register';
 import Login from './components/Login';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-        <Pocetna></Pocetna>
-        <Register></Register>
-        <Login></Login>
-
-        <Footer></Footer>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Pocetna</Link>
+            </li>
+            <li>
+              <Link to="/register">Registracija</Link>
+            </li>
+            <li>
+              <Link to="/login">Prijava</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Routes>
+          <Route path="/" element={<Pocetna />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
